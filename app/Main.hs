6d800adbf031334e426cce51487251b22f835440
Sam Kagan <skagan@nrao.edu>
2022-11-07 17:27:21 -0700

    Implemented Weasel program

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,18 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Weasel (evolve, spawnInitialGeneration)
+import System.Environment (getArgs)
+import System.Random (initStdGen)
 
+-- main <target> [mut-chance] [max-iterations]
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  args <- getArgs
+  let target = head args
+  let mutChance = if length args == 3 then read (args !! 1) else 0.05
+  let numCopies = if length args == 3 then read (args !! 2) else 100
+  rng <- initStdGen
+  let initialGen = spawnInitialGeneration rng (length target)
+  let (generations, rng') = evolve target mutChance numCopies initialGen
+  print $ "It took " ++ show (length generations) ++ " to generate:"
+  print $ last generations
--- src/Weasel.hs
+++ src/Weasel.hs
@@ -1,4 +1,52 @@
-module MyLib (someFunc) where
+module Weasel (spawnInitialGeneration, evolve) where
+import System.Random
+import Data.Bifunctor (first)
+import Data.List (maximumBy)
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+copyAndMutate :: RandomGen g => Float -> Int -> (String, g) -> ([String], g)
+copyAndMutate _ 0 (_, randomGen) = ([], randomGen)
+copyAndMutate probability numCopies (initialCopy, randomGen) = first (copy:) $ copyAndMutate probability (numCopies - 1) (initialCopy, randomGen')
+    where (copy, randomGen') = mutate probability (initialCopy, randomGen)
+
+mutate :: RandomGen g => Float -> (String, g) -> (String, g)
+-- Generate a string identical to organism except each letter has a probability * 100% chance to be changed to a random other letter
+mutate probability (a:rest, randomGen) = first (a':) $ mutate probability (rest, randomGen')
+    where (a', randomGen') = mutateChar probability (a, randomGen)
+mutate _ ([], randomGen) = ("", randomGen)
+
+mutateChar :: RandomGen g => Float -> (Char, g) -> (Char, g)
+mutateChar probability (ch, randomGen)
+    | doMutate = genLetter randomGen 
+    | otherwise = (ch, randomGen')
+    where (doMutate, randomGen') = (fst (roll randomGen) < probability, snd (roll randomGen))
+          roll = uniformR (0 :: Float, 1 :: Float)
+
+-- Fitness is in [0, length expected]
+calculateFitness :: String -> String -> Int
+calculateFitness (e:xpected) (a:ctual)
+    | e == a = 1 + calculateFitness xpected ctual
+    | otherwise = calculateFitness xpected ctual
+calculateFitness _ _ = 0
+
+spawnNextGeneration :: RandomGen g => String -> Float -> Int -> (String, g) -> (String, g)
+spawnNextGeneration target mutChance numCopies current = first (snd . findMostFit) (copyAndMutate mutChance numCopies current)
+    where appendFitness copy = (calculateFitness target copy, copy)
+          findMostFit copies = maximumBy compareByRank $ map appendFitness copies
+          compareByRank (rankA, _) (rankB, _) = compare rankA rankB
+
+evolve :: RandomGen g => String -> Float -> Int -> (String, g) -> ([String], g)
+evolve target mutChance numCopies current
+    | calculateFitness target nextGen == length target = ([nextGen], randomGen)
+    | otherwise = first (nextGen:) $ evolve target mutChance numCopies next
+    where next@(nextGen, randomGen) = spawnNextGeneration target mutChance numCopies current
+
+genLetter :: RandomGen g => g -> (Char, g)
+genLetter randomGen = first intToChar (uniformR (0, 26) randomGen)
+    where intToChar int
+              | int < 26 = ['a'..'z'] !! int
+              | otherwise = ' '
+
+spawnInitialGeneration :: RandomGen g => g -> Int -> (String, g)
+spawnInitialGeneration randomGen 0 = ("", randomGen)
+spawnInitialGeneration randomGen len = first (nextLetter:) $ spawnInitialGeneration randomGen' (len - 1)
+    where (nextLetter, randomGen') = genLetter randomGen
