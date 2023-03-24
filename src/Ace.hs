af83f7ddc65f308070879103664ae7510770a416
Sam Kagan <skagan@nrao.edu>
2023-03-24 17:19:40 -0600

    Refactored Ace.hs to break out more utilities for use in Sexual.hs

--- src/Ace.hs
+++ src/Ace.hs
@@ -1,8 +1,8 @@
-module Ace (spawnInitialGeneration, evolve) where
+module Ace (spawnInitialGeneration, evolve, copyAndMutate) where
 import System.Random
 import Data.Bifunctor (first)
 import Data.List (maximumBy)
-import Util (mutate, calculateFitness, genLetter)
+import Util (mutate, calculateFitness, genLetter, spawnOrganism, findMostFit)
 
 copyAndMutate :: RandomGen g => Float -> Int -> (String, g) -> ([String], g)
 copyAndMutate _ 0 (_, randomGen) = ([], randomGen)
@@ -10,10 +10,7 @@ copyAndMutate probability numCopies (initialCopy, randomGen) = first (copy:) $ c
     where (copy, randomGen') = mutate probability (initialCopy, randomGen)
 
 spawnNextGeneration :: RandomGen g => String -> Float -> Int -> (String, g) -> (String, g)
-spawnNextGeneration target mutChance numCopies current = first (snd . findMostFit) (copyAndMutate mutChance numCopies current)
-    where appendFitness copy = (calculateFitness target copy, copy)
-          findMostFit copies = maximumBy compareByRank $ map appendFitness copies
-          compareByRank (rankA, _) (rankB, _) = compare rankA rankB
+spawnNextGeneration target mutChance numCopies current = first (head . findMostFit target 1) (copyAndMutate mutChance numCopies current)
 
 evolve :: RandomGen g => String -> Float -> Int -> (String, g) -> ([String], g)
 evolve target mutChance numCopies current
@@ -22,6 +19,4 @@ evolve target mutChance numCopies current
     where next@(nextGen, randomGen) = spawnNextGeneration target mutChance numCopies current
 
 spawnInitialGeneration :: RandomGen g => g -> Int -> (String, g)
-spawnInitialGeneration randomGen 0 = ("", randomGen)
-spawnInitialGeneration randomGen len = first (nextLetter:) $ spawnInitialGeneration randomGen' (len - 1)
-    where (nextLetter, randomGen') = genLetter randomGen
+spawnInitialGeneration = spawnOrganism
--- src/Util.hs
+++ src/Util.hs
@@ -1,6 +1,9 @@
-module Util (mutate, calculateFitness, genLetter) where
+module Util (mutate, calculateFitness, genLetter, spawnOrganism, findMostFit) where
 import System.Random (RandomGen, uniformR)
 import Data.Bifunctor (Bifunctor(first))
+import Data.List (maximumBy, sortBy, sortOn)
+import Data.Function (on)
+import Data.Ord
 
 mutate :: RandomGen g => Float -> (String, g) -> (String, g)
 -- Generate a string identical to organism except each letter has a probability * 100% chance to be changed to a random other letter
@@ -25,3 +28,13 @@ genLetter randomGen = first intToChar (uniformR (0, 26) randomGen)
     where intToChar int
               | int < 26 = ['a'..'z'] !! int
               | otherwise = ' '
+
+spawnOrganism :: RandomGen g => g -> Int -> (String, g)
+spawnOrganism randomGen 0 = ("", randomGen)
+spawnOrganism randomGen len = first (nextLetter:) $ spawnOrganism randomGen' (len - 1)
+    where (nextLetter, randomGen') = genLetter randomGen
+
+
+findMostFit :: String -> Int -> [String] -> [String]
+findMostFit target numMostFit copies = map snd $ take numMostFit $ sortOn (Down . fst) $ map (appendFitness target) copies
+    where appendFitness target organism = (calculateFitness target organism, organism)
